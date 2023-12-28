92e52f18f7a596f89a573e8598f6b50415d91ecd
Taalaibek Sagynaliev <sagynalievv.t@gmail.com>
2023-12-28 23:16:03 +0600

    add redux-toolkit and route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
 import './App.css';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 
 const App = () => {
-  return <div>Hello</div>;
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <div style={{ marginBottom: '10px' }}>
+        <button onClick={() => navigate('/')}>Password Access</button>
+        <button
+          onClick={() => navigate('/calculator')}
+          style={{ marginLeft: '10px', backgroundColor: 'blue' }}
+        >
+          Calculator
+        </button>
+      </div>
+      <div>
+        <Routes>
+          <Route path={'/'} element={''} />
+          <Route path={'/calculator'} element={''} />
+          <Route
+            path={'*'}
+            element={<h1 style={{ marginTop: '50px' }}>Not found</h1>}
+          />
+        </Routes>
+      </div>
+    </>
+  );
 };
 
 export default App;
